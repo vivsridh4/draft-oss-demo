c31df6bdbfc440d78a6e425ec03822b65837505a
Vivek Sridhar <vivsridh2@gmail.com>
2022-09-30 12:08:12 +0530

    deploy code

---
+++ demo-app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import tweepy
+from decouple import config
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    
+    import requests
+    response = requests.get("https://vicky007.azurewebsites.net/api/vicky?name=Draft v2 on K8s demo")
+    print(response) 
+    return "Samosa at #OSI2022\n"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
